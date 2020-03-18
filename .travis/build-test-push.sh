ea1678deb8bcc6d74e7fae409848b10c8f26e726
Jonathan Sloan <jsloan117@gmail.com>
2020-03-18 17:59:25 -0500

    updating build script, ignore files, renamed yamllint & prettier files, change docker image tag being pull

--- .travis/build-test-push.sh
+++ .travis/build-test-push.sh
@@ -12,22 +12,36 @@ export IMAGE_TAG
 
 get_version () {
   echo -e '\n<<< Getting & setting versioning info >>>\n'
-  SEMVER_BUMP="${SEMVER_BUMP}"
-  if CURRENT_VERSION=$(docker run --rm "${IMAGE_NAME}":"${IMAGE_TAG}" cat VERSION 2> /dev/null); then
-    echo "${CURRENT_VERSION}" > VERSION
-  fi
   # allows for starting semantic versioning & overriding auto-calculated value (set within TravisCI)
   if [[ -n "${SEMVER_OVERRIDE}" ]]; then
     echo "${SEMVER_OVERRIDE}" > VERSION
+    NEXT_VERSION=$(cat VERSION)
+    echo "Version: ${NEXT_VERSION}"
+  else
+    CURRENT_VERSION=$(docker run --entrypoint="" --rm "${IMAGE_NAME}":"${IMAGE_TAG}" cat VERSION 2> /dev/null)
+    echo "${CURRENT_VERSION}" > VERSION
+    if [[ "${TRAVIS_BRANCH}" = master ]]; then
+      NEXT_VERSION=$(docker run --rm -it -v "${PWD}":/app -w /app treeder/bump --filename VERSION "${SEMVER_BUMP}")
+      echo "Version: ${NEXT_VERSION}"
+    else
+      NEXT_VERSION=$(cat VERSION)
+      echo "Version: ${NEXT_VERSION}"
+    fi
   fi
-  echo "Version: $(docker run --rm -it -v "${PWD}":/app -w /app treeder/bump --filename VERSION "${SEMVER_BUMP}")"
-  NEXT_VERSION=$(cat VERSION)
   export NEXT_VERSION
 }
 
 build_images () {
   echo -e '\n<<< Building default image >>>\n'
-  docker build -f Dockerfile -t "${IMAGE_NAME}":"${IMAGE_TAG}" .
+  docker build --rm -f Dockerfile -t "${IMAGE_NAME}":"${IMAGE_TAG}" .
+  for DISTRO in $(find . -type f -iname "Dockerfile.*" -print | cut -d'/' -f2 | cut -d'.' -f 2); do
+    echo -e "\n<<< Building ${DISTRO} image >>>\n"
+    docker build --rm -f Dockerfile."${DISTRO}" -t "${IMAGE_NAME}":"${IMAGE_TAG}"-"${DISTRO}" .
+  done
+  if docker image ls | tail -n+2 | awk '{print $2}'| grep '<none>' &> /dev/null; then
+    echo -e '\n<<< Cleaning up dangling images >>>\n'
+    docker rmi "$(docker images -f dangling=true -q)" 2>&-
+  fi 
 }
 
 install_prereqs () {
@@ -36,7 +50,7 @@ install_prereqs () {
   GOSS_VER=$(curl -s "https://api.github.com/repos/aelsabbahy/goss/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
   export GOSS_VER
   curl -sL "https://github.com/aelsabbahy/goss/releases/download/v${GOSS_VER}/goss-linux-amd64" -o "${HOME}/bin/goss"
-  curl -sL "https://github.com/aelsabbahy/goss/releases/download/v${GOSS_VER}/dgoss" -o "$HOME/bin/dgoss"
+  curl -sL "https://github.com/aelsabbahy/goss/releases/download/v${GOSS_VER}/dgoss" -o "${HOME}/bin/dgoss"
   # trivy (vuln scanner)
   TRIVY_VER=$(curl -s "https://api.github.com/repos/aquasecurity/trivy/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
   export TRIVY_VER
@@ -45,33 +59,51 @@ install_prereqs () {
   # snyk (vuln scanner)
   SNYK_VER=$(curl -s "https://api.github.com/repos/snyk/snyk/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
   export SNYK_VER
-  curl -sL "https://github.com/snyk/snyk/releases/download/v$SNYK_VER/snyk-linux" -o "$HOME/bin/snyk"
-  chmod +rx "$HOME"/bin/{goss,dgoss,snyk}
+  curl -sL "https://github.com/snyk/snyk/releases/download/v${SNYK_VER}/snyk-linux" -o "${HOME}/bin/snyk"
+  chmod +rx "${HOME}"/bin/{goss,dgoss,snyk}
 }
 
 vulnerability_scanner () {
-  echo -e '\n<<< Checking image for vulnerabilities >>>\n'
   trivy --clear-cache
-  trivy --exit-code 0 --severity "UNKNOWN,LOW,MEDIUM,HIGH" --no-progress "${IMAGE_NAME}":"${IMAGE_TAG}"
-  trivy --exit-code 1 --severity CRITICAL --no-progress "${IMAGE_NAME}":"${IMAGE_TAG}"
-  if [[ "${TRAVIS_BRANCH}" = master ]]; then
-    snyk auth "${SNYK_TOKEN}" &> /dev/null
-    snyk monitor --docker "${IMAGE_NAME}":"${IMAGE_TAG}" --file=Dockerfile
-  fi
+  for IMAGE in $(docker image ls | tail -n+2 | awk '{OFS=":";} {print $1,$2}'| grep "${DOCKER_USER}"); do
+    echo -e "\n<<< Checking ${IMAGE} for vulnerabilities >>>\n"
+    trivy --exit-code 0 --severity "UNKNOWN,LOW,MEDIUM,HIGH" --light -q "${IMAGE}"
+    echo -e "\n<<< Checking ${IMAGE} for critical vulnerabilities >>>\n"
+    trivy --exit-code 1 --severity CRITICAL --light -q "${IMAGE}"
+    if [[ "${TRAVIS_PULL_REQUEST}" = false ]] && [[ "${TRAVIS_BRANCH}" = master ]]; then
+      snyk auth "${SNYK_TOKEN}" &> /dev/null
+      snyk monitor --docker "${IMAGE_NAME}":"${IMAGE_TAG}" --file=Dockerfile
+      for DISTRO in $(find . -type f -iname "Dockerfile.*" -print | cut -d'/' -f2 | cut -d'.' -f 2); do
+        snyk monitor --docker "${IMAGE_NAME}":"${IMAGE_TAG}"-"${DISTRO}" --file=Dockerfile."${DISTRO}"
+      done
+    fi
+  done
 }
 
 test_images () {
-  echo -e '\n<<< Testing default image >>>\n'
-  export GOSS_SLEEP=5
-  dgoss run -e PUID=1000 -e PGID=1000 "${IMAGE_NAME}":"${IMAGE_TAG}"
+  for IMAGE in $(docker image ls | tail -n+2 | awk '{OFS=":";} {print $1,$2}'| grep "${DOCKER_USER}"); do
+    echo -e "\n<<< Testing ${IMAGE} image >>>\n"
+    dgoss run "${IMAGE_NAME}":"${IMAGE_TAG}"
+  done
 }
 
 push_images () {
   echo "${DOCKER_PASS}" | docker login -u "${DOCKER_USER}" --password-stdin &> /dev/null
-  echo -e '\n<<< Pushing default image >>>\n'
-  docker tag "${IMAGE_NAME}":"${IMAGE_TAG}" "${IMAGE_NAME}":"${NEXT_VERSION}"
-  docker push "${IMAGE_NAME}":"${IMAGE_TAG}"
-  docker push "${IMAGE_NAME}":"${NEXT_VERSION}"
+  if [[ "${TRAVIS_BRANCH}" = dev ]]; then
+    for IMAGE in $(docker image ls | tail -n+2 | awk '{OFS=":";} {print $1,$2}'| grep "${DOCKER_USER}"); do
+      echo -e "\n<<< Pushing ${IMAGE} image >>>\n"
+      docker push "${IMAGE}"
+    done
+  elif [[ "${TRAVIS_BRANCH}" = master ]]; then
+    docker tag "${IMAGE_NAME}":"${IMAGE_TAG}" "${IMAGE_NAME}":"${NEXT_VERSION}"
+    for DISTRO in $(find . -type f -iname "Dockerfile.*" -print | cut -d'/' -f2 | cut -d'.' -f 2); do
+      docker tag "${IMAGE_NAME}":"${IMAGE_TAG}"-"${DISTRO}" "${IMAGE_NAME}":"${NEXT_VERSION}"-"${DISTRO}"
+    done
+    for IMAGE in $(docker image ls | tail -n+2 | awk '{OFS=":";} {print $1,$2}'| grep "${DOCKER_USER}"); do
+      echo -e "\n<<< Pushing ${IMAGE} image >>>\n"
+      docker push "${IMAGE}"
+    done  
+  fi
 }
 
 get_version
@@ -80,7 +112,9 @@ install_prereqs
 if [[ "${VULNERABILITY_TEST}" = true ]]; then
   vulnerability_scanner
 fi
-test_images
+if [[ "${DGOSS_TEST}" = true ]]; then
+  test_images
+fi
 if [[ "${TRAVIS_PULL_REQUEST}" = false ]] && [[ "${TRAVIS_BRANCH}" =~ ^(dev|master)$ ]]; then
   push_images
 fi
--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM telegraf:latest
+FROM telegraf:1.13
 LABEL Name=telegraf maintainer="Jonathan Sloan"
 
 RUN apt-get update && apt-get install -y --no-install-recommends hddtemp smartmontools apcupsd \
